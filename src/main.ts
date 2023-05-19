a3b743c7c346e422f7f4d791193918ebef059417
ranyong <ranyong1209@gmail.com>
2023-05-19 16:35:23 +0800

    feat: 模版的路由配置

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,23 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 //配置element-plus国际化
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 引入模板的全局样式
+import '@/style/index.scss'
+// svg插件需要配置代码
+import 'virtual:svg-icons-register'
+// 引入自定义插件：注册整个项目组件
+import gloalComponent from '@/components'
+// 引入路由
+import router from './router'
 //获取应用实例对象
 const app = createApp(App)
 //安装element-plus插件
 app.use(ElementPlus, {
   locale: zhCn, //element-plus国际化配置
 })
-// svg插件需要配置代码
-import 'virtual:svg-icons-register'
-// 引入自定义插件：注册整个项目组件
-import gloalComponent from '@/components'
 // 安装自定义插件
 app.use(gloalComponent)
-// 引入模板的全局样式
-import '@/style/index.scss'
+// 注册模版路由
+app.use(router)
 //将应用挂载到挂载点上
 app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+// 通过vue-router插件实现模版路由配置
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { constantRouter } from './router'
+// 创建路由器
+let router = createRouter({
+    // 路由模式hash
+    history: createWebHashHistory(),
+    routes: constantRouter,
+    // 滚动行为
+    scrollBehavior() {
+        return {
+            left: 0,
+            top: 0
+        }
+    }
+})
+export default router;
