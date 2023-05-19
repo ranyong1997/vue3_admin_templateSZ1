8deec57f4d12801ab2390b2f5eeaee56cfd1a64e
ranyong <ranyong1209@gmail.com>
2023-05-19 14:31:34 +0800

    feat: axios二次封装

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,52 +1,55 @@
 // 进行axios二次封装：使用请求与响应拦截器
-import axios from 'axios';
+import axios from 'axios'
 import { ElMessage } from 'element-plus'
 // 第一步：利用axios对象create方法，去创建axios实例
 let request = axios.create({
-    // 基础路径
-    baseURL: import.meta.env.VITE_APP_BASE_API,//基础路径会携带/api
-    timeout: 5000//超时的时间的设置
-});
+  // 基础路径
+  baseURL: import.meta.env.VITE_APP_BASE_API, //基础路径会携带/api
+  timeout: 5000, //超时的时间的设置
+})
 // 第二步：request实例添加请求与响应拦截器
 request.interceptors.request.use((config) => {
-    // config配置对象，headers属性请求头，经常给服务器端携带公共参数
-    // 返回配置对象
-    return config;
+  // config配置对象，headers属性请求头，经常给服务器端携带公共参数
+  // 返回配置对象
+  return config
 })
 // 第三步：响应拦截器
-request.interceptors.response.use((response) => {
+request.interceptors.response.use(
+  (response) => {
     // 成功回调
     // 简化数据
-    return response.data;
-}, (error) => {
+    return response.data
+  },
+  (error) => {
     // 失败回调:处理http网络错误的
     // 定义一个变量:存储一个网络错误信息
-    let message = '';
+    let message = ''
     // http状态码
-    let status = error.response.status;
+    let status = error.response.status
     switch (status) {
-        case 401:
-            message = "TOKEN过期"
-            break;
-        case 403:
-            message = "无权访问"
-            break;
-        case 404:
-            message = "请求地址错误"
-            break;
-        case 500:
-            message = "服务器出现问题"
-            break;
-        default:
-            message = "网络出现问题"
-            break;
+      case 401:
+        message = 'TOKEN过期'
+        break
+      case 403:
+        message = '无权访问'
+        break
+      case 404:
+        message = '请求地址错误'
+        break
+      case 500:
+        message = '服务器出现问题'
+        break
+      default:
+        message = '网络出现问题'
+        break
     }
     // 提示错误信息
     ElMessage({
-        type: 'error',
-        message
+      type: 'error',
+      message,
     })
-    return Promise.reject(error);
-})
+    return Promise.reject(error)
+  },
+)
 // 对外暴漏
-export default request;
+export default request
