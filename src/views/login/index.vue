4340b542746ece2923660168f82d6361bab09bea
ranyong <ranyong1209@gmail.com>
2023-05-22 16:40:15 +0800

    feat: 自定义校验规则表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,17 +3,36 @@
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
+        <el-form
+          class="login_form"
+          :model="loginForm"
+          :rules="rules"
+          ref="loginForms"
+        >
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
           <el-form-item prop="username">
-            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+            <el-input
+              :prefix-icon="User"
+              v-model="loginForm.username"
+            ></el-input>
           </el-form-item>
           <el-form-item prop="password">
-            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+            <el-input
+              type="password"
+              :prefix-icon="Lock"
+              v-model="loginForm.password"
+              show-password
+            ></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">
+            <el-button
+              :loading="loading"
+              class="login_btn"
+              type="primary"
+              size="default"
+              @click="login"
+            >
               登录
             </el-button>
           </el-form-item>
@@ -103,12 +122,12 @@ const rules = {
   // trigger:触发校验表单的时机，change->文本发生变化触发校验,blur->失去焦点额时候触发校验规则
   username: [
     // { required: true, min: 5, max: 10, message: '账号长度至少5位', trigger: 'change' },
-    { trigger: 'change', validator: validatorUserName }
+    { trigger: 'change', validator: validatorUserName },
   ],
   password: [
     // { required: true, min: 6, max: 15, message: '密码长度至少6位', trigger: 'change' },
-    { trigger: 'change', validator: validatorPassWord }
-  ]
+    { trigger: 'change', validator: validatorPassWord },
+  ],
 }
 </script>
 
