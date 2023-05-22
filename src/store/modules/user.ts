1b1a2a07ea72597028496874fbb797838bd02002
ranyong <ranyong1209@gmail.com>
2023-05-22 14:17:54 +0800

    feat: 封装登录业务

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,13 +1,38 @@
 // 创建用户相关的小仓库
 import { defineStore } from 'pinia'
+// 引入登录接口
+import { reqLogin } from '@/api/user'
+// 引入数据类型
+import type { loginForm } from '@/api/user/type'
 // 创建用户小仓库
 const useUserStore = defineStore('User', {
   // 小仓库存储数据地方
   state: () => {
-    return {}
+    return {
+      token: localStorage.getItem("TOKEN") // 用户唯一标识
+    }
   },
   // 异步|逻辑地方
-  actions: {},
+  actions: {
+    // 用户登录的方法
+    async userLogin(data: loginForm) {
+      // 登录请求
+      let result:any = await reqLogin(data);
+      // 登录请求：成功200 -> token
+      // 登录请求：失败201 -> 登录失败错误信息
+      if (result.code == 200) {
+        // pinia仓库存储一下token
+        this.token = result.data.token;
+        // 本地存储持久化存储一份
+        localStorage.setItem("TOKEN", result.data.token)
+        // 保证当前async函数返回一个成功的promise
+        return 'ok';
+      } else {
+        return Promise.reject(new Error(result.data.message)) 
+
+      }
+    }
+  },
   getters: {},
 })
 // 对外暴露获取小仓库方法
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,26 +7,13 @@
           <h1>Hello</h1>
           <h2>欢迎来到硅谷甄选</h2>
           <el-form-item>
-            <el-input
-              :prefix-icon="User"
-              v-model="loginForm.username"
-            ></el-input>
+            <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-input
-              type="password"
-              :prefix-icon="Lock"
-              v-model="loginForm.password"
-              show-password
-            ></el-input>
+            <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button
-              class="login_btn"
-              type="primary"
-              size="default"
-              @click="login"
-            >
+            <el-button :loading="loading" class="login_btn" type="primary" size="default" @click="login">
               登录
             </el-button>
           </el-form-item>
@@ -39,17 +26,49 @@
 <script setup lang="ts">
 import { User, Lock } from '@element-plus/icons-vue'
 // 收集账号与密码的数据
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+// 引入element-plus中ElNotification
+import { ElNotification } from 'element-plus'
 // 引入用户相关的小仓库
 import userUserStore from '@/store/modules/user'
+import { tr } from 'element-plus/es/locale'
+// 定义变量控制按钮加载效果
+let loading = ref(false)
 let useStore = userUserStore()
-
+// 获取路径
+let $router = useRouter()
+// 收集账号与密码的数据
 let loginForm = reactive({
   username: 'admin',
   password: '111111',
 })
 // 登录按钮回调
-const login = () => {}
+const login = async () => {
+  // 开始加载
+  loading.value = true
+  try {
+    // 保证登录成功
+    await useStore.userLogin(loginForm);
+    //  编程式导航跳转到展示数据首页
+    $router.push('/')
+    // 登录成功提示信息
+    ElNotification({
+      type: 'success',
+      message: '登录成功'
+    })
+    // 登录成功加载效果也消失
+    loading.value = false
+  } catch (error) {
+    // 登录失败加载效果消失
+    loading.value = false
+    // 登录失败提示信息
+    ElNotification({
+      type: 'error',
+      message: (error as Error).message
+    })
+  }
+}
 </script>
 
 <style scoped lang="scss">
