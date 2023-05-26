49fa97751aefc8501a50d3538200b03b3b09fa85
ranyong <ranyong1209@gmail.com>
2023-05-26 15:11:32 +0800

    feat: 品牌管理模块数据展示

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    <h1>品牌管理</h1>
-  </div>
+  <el-card class="box-card">
+    <!-- 卡片顶部添加品牌按钮 -->
+    <el-button type="primary" icon="Plus">Primary</el-button>
+    <!-- 表格组件：用于展示已有的平台数据 -->
+    <el-table style="margin: 10px 0px;" border>
+      <el-table-column label="序号" width="80px" align="center"></el-table-column>
+      <el-table-column label="品牌名称"></el-table-column>
+      <el-table-column label="品牌LOGO"></el-table-column>
+      <el-table-column label="品牌操作"></el-table-column>
+    </el-table>
+    <!-- 分页器 -->
+    <el-pagination v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
+      layout="prev, pager, next, jumper, ->, sizes, total," :total="400" />
+  </el-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 引入组合式API函数ref
+import { ref } from 'vue'
+// 当前页码
+let pageNo = ref<number>(1)
+// 每一页展示多少条数据
+let limit = ref<number>(3)
+</script>
 
 <style scoped></style>
