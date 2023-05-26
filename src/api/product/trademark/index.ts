cda538cef4e93aaf787ee3db6a59c4cc88206bdd
ranyong <ranyong1209@gmail.com>
2023-05-26 17:04:14 +0800

    feat: 已有品牌ts类型

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,5 +1,6 @@
 // 书写品牌管理模块接口
 import request from '@/utils/request'
+import type { TradeMarkResponseData } from './type'
 // 品牌管理模块接口地址
 enum API {
   // 获取已有的品牌接口
@@ -9,4 +10,4 @@ enum API {
 // page:获取第几页 -- 默认第一页
 // limit:获取几个已有品牌的数据
 export const reqHasTrademark = (page: number, limit: number) =>
-  request.get<any, any>(API.TRADEMAEK_URL + `${page}/${limit}`)
+  request.get<any, TradeMarkResponseData>(API.TRADEMAEK_URL + `${page}/${limit}`)
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -4,12 +4,7 @@
     <el-button type="primary" icon="Plus">添加品牌</el-button>
     <!-- 表格组件：用于展示已有的平台数据 -->
     <el-table style="margin: 10px 0px" border :data="trademarkArr">
-      <el-table-column
-        label="序号"
-        width="80px"
-        align="center"
-        type="index"
-      ></el-table-column>
+      <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
       <el-table-column label="品牌名称" align="center">
         <template #="{ row, $index }">
           <pre>{{ row.tmName }}</pre>
@@ -28,14 +23,8 @@
       </el-table-column>
     </el-table>
     <!-- 分页器 -->
-    <el-pagination
-      v-model:current-page="pageNo"
-      v-model:page-size="limit"
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true"
-      layout="prev, pager, next, jumper, ->, sizes, total,"
-      :total="total"
-    />
+    <el-pagination v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[3, 5, 7, 9]" :background="true"
+      layout="prev, pager, next, jumper, ->, sizes, total," :total="total" />
   </el-card>
 </template>
 
@@ -43,6 +32,7 @@
 // 引入组合式API函数ref
 import { ref, onMounted } from 'vue'
 import { reqHasTrademark } from '@/api/product/trademark'
+import type { Records, TradeMarkResponseData } from '@/api/product/trademark/type'
 // 当前页码
 let pageNo = ref<number>(1)
 // 每一页展示多少条数据
@@ -50,10 +40,10 @@ let limit = ref<number>(3)
 // 存储已有品牌数据总数
 let total = ref<number>(0)
 // 存储已有品牌的数据
-let trademarkArr = ref<any>([])
+let trademarkArr = ref<Records>([])
 // 获取已有品牌的接口封装为一个函数
 const getHasTrademark = async () => {
-  let result = await reqHasTrademark(pageNo.value, limit.value)
+  let result: TradeMarkResponseData = await reqHasTrademark(pageNo.value, limit.value)
   if (result.code == 200) {
     // 存储已有品牌总个数
     total.value = result.data.total
