ddc46b904726cad9ffdb4a4cee71b2f364a6a71f
ranyong <ranyong1209@gmail.com>
2023-06-09 17:30:08 +0800

    feat: 平台属性管理模块静态搭建

--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -1,36 +1,34 @@
 <template>
-    <el-card>
-        <el-form :inline="true">
-            <el-form-item label="一级分类">
-                <el-select>
-                    <el-option label="北京"></el-option>
-                    <el-option label="上海"></el-option>
-                    <el-option label="广州"></el-option>
-                    <el-option label="深圳"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="二级分类">
-                <el-select>
-                    <el-option label="北京"></el-option>
-                    <el-option label="上海"></el-option>
-                    <el-option label="广州"></el-option>
-                    <el-option label="深圳"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="三级分类">
-                <el-select>
-                    <el-option label="北京"></el-option>
-                    <el-option label="上海"></el-option>
-                    <el-option label="广州"></el-option>
-                    <el-option label="深圳"></el-option>
-                </el-select>
-            </el-form-item>
-        </el-form>
-    </el-card>
+  <el-card>
+    <el-form :inline="true">
+      <el-form-item label="一级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select>
+          <el-option label="北京"></el-option>
+          <el-option label="上海"></el-option>
+          <el-option label="广州"></el-option>
+          <el-option label="深圳"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <!-- 三级分类全局组件 -->
-    <Category/>
+    <Category />
     <el-card style="margin: 10px 0px">
       <el-button type="primary" size="default" icon="Plus">添加属性</el-button>
-      <el-table border style="margin:10px 0px">
-        <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+      <el-table border style="margin: 10px 0px">
+        <el-table-column
+          label="序号"
+          type="index"
+          align="center"
+          width="80px"
+        ></el-table-column>
         <el-table-column label="属性名称" width="120px"></el-table-column>
         <el-table-column label="属性值名称"></el-table-column>
         <el-table-column label="操作" width="120px"></el-table-column>
