c84c0d863ee0590590e4cb6d85b8a6479a50de14
ranyong <ranyong1209@gmail.com>
2023-06-19 15:14:24 +0800

    feat: 添加与修改属性静态搭建

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -4,16 +4,34 @@
     <Category :scene="scene" />
     <el-card style="margin: 10px 0px">
       <div v-show="scene == 0">
-        <el-button @click="addAttr" type="primary" size="default" icon="Plus"
-          :disabled="categoryStore.c3Id ? false : true">
+        <el-button
+          @click="addAttr"
+          type="primary"
+          size="default"
+          icon="Plus"
+          :disabled="categoryStore.c3Id ? false : true"
+        >
           添加属性
         </el-button>
         <el-table border style="margin: 10px 0px" :data="attrArr">
-          <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
-          <el-table-column label="属性名称" width="120px" prop="attrName"></el-table-column>
+          <el-table-column
+            label="序号"
+            type="index"
+            align="center"
+            width="80px"
+          ></el-table-column>
+          <el-table-column
+            label="属性名称"
+            width="120px"
+            prop="attrName"
+          ></el-table-column>
           <el-table-column label="属性值名称">
             <template #="{ row, $index }">
-              <el-tag style="margin: 5px" v-for="(item, index) in row.attrValueList" :key="item.id">
+              <el-tag
+                style="margin: 5px"
+                v-for="(item, index) in row.attrValueList"
+                :key="item.id"
+              >
                 {{ item.valueName }}
               </el-tag>
             </template>
@@ -21,8 +39,17 @@
           <el-table-column label="操作" width="200px">
             <!-- row:已有属性对象 -->
             <template #="{ row, $index }">
-              <el-button type="primary" size="small" icon="Edit" @click="updateAttr">编辑</el-button>
-              <el-button type="danger" size="small" icon="Delete">删除</el-button>
+              <el-button
+                type="primary"
+                size="small"
+                icon="Edit"
+                @click="updateAttr"
+              >
+                编辑
+              </el-button>
+              <el-button type="danger" size="small" icon="Delete">
+                删除
+              </el-button>
             </template>
           </el-table-column>
         </el-table>
@@ -34,10 +61,17 @@
             <el-input placeholder="请你输入属性名称"></el-input>
           </el-form-item>
         </el-form>
-        <el-button type="primary" size="default" icon="Plus">添加属性值</el-button>
+        <el-button type="primary" size="default" icon="Plus">
+          添加属性值
+        </el-button>
         <el-button size="default" @click="cancel">取消</el-button>
-        <el-table border style="margin:10px 0px">
-          <el-table-column label="序号" width="80px" type="index" align="center"></el-table-column>
+        <el-table border style="margin: 10px 0px">
+          <el-table-column
+            label="序号"
+            width="80px"
+            type="index"
+            align="center"
+          ></el-table-column>
           <el-table-column label="属性值"></el-table-column>
           <el-table-column label="属性值操作"></el-table-column>
         </el-table>
